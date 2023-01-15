1d0e8e9e145e6a1f88aed2d6df867bfc9bef61c1
itsevgenii <smazanovich@icloud.com>
2023-01-15 22:54:18 +0000

    changed class name

--- src/Components/BtnGitHub/BtnGitHub.jsx
+++ src/Components/BtnGitHub/BtnGitHub.jsx
@@ -3,7 +3,7 @@ import "./BtnGitHub.css";
 import gitHubIcon from "../../img/icons/gitHub-black.svg";
 const BtnGitHub = ({ link }) => {
   return (
-    <a href={link} target="_blank" className="btn-outline">
+    <a href={link} target="_blank" className="btnd-outline">
       <img src={gitHubIcon} alt="" />
       GitHub repo
     </a>
