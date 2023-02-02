bc5209f152701243c6d1870a456fea51df2703e9
itsevgenii <smazanovich@icloud.com>
2023-02-02 14:46:15 +0000

    added new project

--- src/Components/Helpers/projectsList.jsx
+++ src/Components/Helpers/projectsList.jsx
@@ -7,17 +7,8 @@ import project02Big from "../../img/projects/BonsaiProject2Big.png";
 import project03 from "../../img/projects/03-WeatherChecker.png";
 import project03Big from "../../img/projects/03-WeatherCheckerBig.png";
 
-// import project04 from "../../img/projects/04-Hobi.png";
-// import project04Big from "../../img/projects/04-Hobi.png";
-
-// import project04 from "../../img/projects/04.jpg";
-// import project04Big from "../../img/projects/04-big.jpg";
-
-// import project05 from "../../img/projects/05.jpg";
-// import project05Big from "../../img/projects/05-big.jpg";
-
-// import project06 from "../../img/projects/06.jpg";
-// import project06Big from "../../img/projects/06-big.jpg";
+import project04 from "../../img/projects/04-hobi1.png";
+import project04Big from "../../img/projects/04-hobi2.png";
 
 const projectsList = [
   {
@@ -43,12 +34,12 @@ const projectsList = [
     gitHubLink: "https://63aaf2869c74230009e7ced9--itsevgenii.netlify.app",
   },
   {
-    title: "Hobi - Daily, 5 days weather forecast",
-    img: project03Big,
-    imgBig: project03,
+    title: "Hobi - Skills' sharing platform",
+    img: project04Big,
+    imgBig: project04,
     skills:
-      "Netlify deployment, React, React Routes, Auth0, TypeScript, Liquidity, API fetching",
-    gitHubLink: "https://63aaf2869c74230009e7ced9--itsevgenii.netlify.app",
+      "AWS Amplify and Render deployment, NextJS, NodeJS, ExpressJS, Tailwind, MongoDB, Auth0",
+    gitHubLink: "https://main.d2wcmuqqs91fni.amplifyapp.com",
   },
   // {
   //   title: "Dating app",
